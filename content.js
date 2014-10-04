caa170b2cb5c47b83553ac2f2a4e64393ad4ed8b
David Altmayer <davidalt@gmail.com>
2014-10-05 00:14:23 +0200

    Updated for diply
    Updated for Diply.com -> ver: 0.1.2

--- content.js
+++ content.js
@@ -1,117 +1,195 @@
 window.onload = function(){
 	var theURL = document.URL;
 	var p2URL, p3URL, p4URL, p5URL;
-	if (theURL.substring(theURL.length - 1, theURL.length) == "/"){
-		p2URL = theURL.substring(0, theURL.length - 1) + "_p2/";
-		p3URL = theURL.substring(0, theURL.length - 1) + "_p3/";
-		p4URL = theURL.substring(0, theURL.length - 1) + "_p4/";
-		p5URL = theURL.substring(0, theURL.length - 1) + "_p5/";
+	var domain = document.domain;
+	if ((domain == "www.cracked.com") || (domain == "cracked.com")){
+		domain = "cracked";
 	}
-	else {
-		p2URL = theURL.substring(0, theURL.length - 5) + "_p2.html";
-		p3URL = theURL.substring(0, theURL.length - 5) + "_p3.html";
-		p4URL = theURL.substring(0, theURL.length - 5) + "_p4.html";
-		p5URL = theURL.substring(0, theURL.length - 5) + "_p5.html";
+	else if ((domain == "www.diply.com") || (domain == "diply.com")){
+		domain = "diply";
 	}
-
-	var request2 = new XMLHttpRequest();
-	var request3 = new XMLHttpRequest();
-	var request4 = new XMLHttpRequest();
-	var request5 = new XMLHttpRequest();
-
-	request2.addEventListener("load", function(evt){
-		console.log(evt);
-	}, false);
-	request3.addEventListener("load", function(evt){
-		console.log(evt);
-	}, false);
-	request4.addEventListener("load", function(evt){
-		console.log(evt);
-	}, false);
-	request5.addEventListener("load", function(evt){
-		console.log(evt);
-	}, false);
-
-	var x = document.getElementsByTagName('section');
-	var bodysection;
-
-	for (var i=0; i < 6; i++){
-		if (x[i].className == "body"){
-			bodysection = x[i];
+	
+	if (domain == "cracked"){
+	
+		if (theURL.substring(theURL.length - 1, theURL.length) == "/"){
+			p2URL = theURL.substring(0, theURL.length - 1) + "_p2/";
+			p3URL = theURL.substring(0, theURL.length - 1) + "_p3/";
+			p4URL = theURL.substring(0, theURL.length - 1) + "_p4/";
+			p5URL = theURL.substring(0, theURL.length - 1) + "_p5/";
 		}
-	}
-	var p1content = bodysection.getElementsByTagName('section')[0];
-	var p2content, p3content, p4content, p5content;
+		else {
+			p2URL = theURL.substring(0, theURL.length - 5) + "_p2.html";
+			p3URL = theURL.substring(0, theURL.length - 5) + "_p3.html";
+			p4URL = theURL.substring(0, theURL.length - 5) + "_p4.html";
+			p5URL = theURL.substring(0, theURL.length - 5) + "_p5.html";
+		}
 
-	request2.onreadystatechange=function(){
-		if (request2.readyState==4 && request2.status==200){
-			var page2 = request2.response;
-			var x2 = page2.getElementsByTagName('section');
-			var bodysection2;
+		var request2 = new XMLHttpRequest();
+		var request3 = new XMLHttpRequest();
+		var request4 = new XMLHttpRequest();
+		var request5 = new XMLHttpRequest();
 
-			for (var i=0; i < 6; i++){
-				if (x2[i].className == "body"){
-					bodysection2 = x2[i];
-				}
+		request2.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+		request3.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+		request4.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+		request5.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+
+		var x = document.getElementsByTagName('section');
+		var bodysection;
+
+		for (var i=0; i < 6; i++){
+			if (x[i].className == "body"){
+				bodysection = x[i];
 			}
-			p2content = bodysection2.getElementsByTagName('section')[0];
-			var newbody = p1content.innerHTML + p2content.innerHTML;
-			
-			request3.onreadystatechange=function(){
-				if (request3.readyState==4 && request3.status==200){
-					var page3 = request3.response;
-					var x3 = page3.getElementsByTagName('section');
-					var bodysection3;
+		}
+		var p1content = bodysection.getElementsByTagName('section')[0];
+		var p2content, p3content, p4content, p5content;
 
-					for (var i=0; i < 6; i++){
-						if (x3[i].className == "body"){
-							bodysection3 = x3[i];
-						}
-					}
-					p3content = bodysection3.getElementsByTagName('section')[0];
-					newbody = newbody + p3content.innerHTML;
-				}
-			}
-			
-			request4.onreadystatechange=function(){
-				if (request4.readyState==4 && request4.status==200){
-					var page4 = request4.response;
-					var x4 = page4.getElementsByTagName('section');
-					var bodysection4;
+		request2.onreadystatechange=function(){
+			if (request2.readyState==4 && request2.status==200){
+				var page2 = request2.response;
+				var x2 = page2.getElementsByTagName('section');
+				var bodysection2;
 
-					for (var i=0; i < 6; i++){
-						if (x4[i].className == "body"){
-							bodysection4 = x4[i];
-						}
+				for (var i=0; i < 6; i++){
+					if (x2[i].className == "body"){
+						bodysection2 = x2[i];
 					}
-					p4content = bodysection4.getElementsByTagName('section')[0];
-					newbody = newbody + p4content.innerHTML;
 				}
-			}
+				p2content = bodysection2.getElementsByTagName('section')[0];
+				var newbody = p1content.innerHTML + p2content.innerHTML;
 			
-			request5.onreadystatechange=function(){
-				if (request5.readyState==4 && request5.status==200){
-					var page5 = request5.response;
-					var x5 = page5.getElementsByTagName('section');
-					var bodysection5;
+				request3.onreadystatechange=function(){
+					if (request3.readyState==4 && request3.status==200){
+						var page3 = request3.response;
+						var x3 = page3.getElementsByTagName('section');
+						var bodysection3;
 
-					for (var i=0; i < 6; i++){
-						if (x5[i].className == "body"){
-							bodysection5 = x5[i];
+						for (var i=0; i < 6; i++){
+							if (x3[i].className == "body"){
+								bodysection3 = x3[i];
+							}
 						}
+						p3content = bodysection3.getElementsByTagName('section')[0];
+						newbody = newbody + p3content.innerHTML;
 					}
-					p5content = bodysection5.getElementsByTagName('section')[0];
-					newbody = newbody + p5content.innerHTML;
 				}
+			
+				request4.onreadystatechange=function(){
+					if (request4.readyState==4 && request4.status==200){
+						var page4 = request4.response;
+						var x4 = page4.getElementsByTagName('section');
+						var bodysection4;
+
+						for (var i=0; i < 6; i++){
+							if (x4[i].className == "body"){
+								bodysection4 = x4[i];
+							}
+						}
+						p4content = bodysection4.getElementsByTagName('section')[0];
+						newbody = newbody + p4content.innerHTML;
+					}
+				}
+			
+				request5.onreadystatechange=function(){
+					if (request5.readyState==4 && request5.status==200){
+						var page5 = request5.response;
+						var x5 = page5.getElementsByTagName('section');
+						var bodysection5;
+
+						for (var i=0; i < 6; i++){
+							if (x5[i].className == "body"){
+								bodysection5 = x5[i];
+							}
+						}
+						p5content = bodysection5.getElementsByTagName('section')[0];
+						newbody = newbody + p5content.innerHTML;
+					}
 				
-				p1content.innerHTML = newbody;
+					p1content.innerHTML = newbody;
 			
-				// fix images that don't show up because of stupid use of data-img propertyx
-				document.body.innerHTML = document.body.innerHTML.replace(/data-img/g,'src');
+					// fix images that don't show up because of stupid use of data-img propertyx
+					document.body.innerHTML = document.body.innerHTML.replace(/data-img/g,'src');
+				}
+			
 			}
-			
 		}
 	}
+	else if (domain == "diply"){
+	
+		p2URL = theURL.substring(0, theURL.length) + "/2";
+		p3URL = theURL.substring(0, theURL.length) + "/3";
+		p4URL = theURL.substring(0, theURL.length) + "/4";
+		p5URL = theURL.substring(0, theURL.length) + "/5";
+
+		var request2 = new XMLHttpRequest();
+		var request3 = new XMLHttpRequest();
+		var request4 = new XMLHttpRequest();
+		var request5 = new XMLHttpRequest();
+
+		request2.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+		request3.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+		request4.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+		request5.addEventListener("load", function(evt){
+			console.log(evt);
+		}, false);
+
+		var p1content = document.getElementsByClassName('card')[0];
+		var p2content, p3content, p4content, p5content;
+
+		request2.onreadystatechange=function(){
+			if (request2.readyState==4 && request2.status==200){
+				var page2 = request2.response;
+				p2content = page2.getElementsByClassName('card')[0];
+				var newbody = p1content.innerHTML + p2content.innerHTML;
+			
+				request3.onreadystatechange=function(){
+					if (request3.readyState==4 && request3.status==200){
+						var page3 = request3.response;
+						p3content = page3.getElementsByClassName('card')[0];
+						newbody = newbody + p3content.innerHTML;
+					}
+				}
+			
+				request4.onreadystatechange=function(){
+					if (request4.readyState==4 && request4.status==200){
+						var page4 = request4.response;
+						p4content = page4.getElementsByClassName('card')[0];
+						newbody = newbody + p4content.innerHTML;
+					}
+				}
+			
+				request5.onreadystatechange=function(){
+					if (request5.readyState==4 && request5.status==200){
+						var page5 = request5.response;
+						p5content = page5.getElementsByClassName('card')[0];
+						newbody = newbody + p5content.innerHTML;
+					}
+				
+					p1content.innerHTML = newbody;
+			
+					// fix images that don't show up because of stupid use of data-img propertyx
+					document.body.innerHTML = document.body.innerHTML.replace(/data-img/g,'src');
+				}
+			
+			}
+		}
+	}
+	
   
 	request2.open('GET', p2URL, true);
 	request2.responseType = "document";
